7a251cc24430f7f8d388b4f81284cc194d2276b7
Deependra Rawat <rdeependra33@gmail.com>
2022-09-09 15:18:14 +0530

    first

--- detect_image/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DetectImageConfig(AppConfig):
-    name = 'detect_image'
--- detect_image/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.views.static import serve
-
-urlpatterns = [
-    url(r'^detect_image/(?P<path>.*)$', serve, {
-        'document_root': settings.MEDIA_ROOT,
-    }),
-]
